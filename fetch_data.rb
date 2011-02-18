341748570324ac94f954c5188123e6d39599630f
Matt Zukowski <matt@zukowski.ca>
2011-02-18 15:53:50 -0500

    better error reporting when data fetching fails

--- fetch_data.rb
+++ fetch_data.rb
@@ -11,6 +11,12 @@ puts "Logging in to #{tf}..."
 tf.login!
 puts "Getting positions from #{tf}..."
 tf_positions = tf.get_positions
+
+if tf_positions.size < 1
+  $stderr.puts "FAILED TO GET DATA FROM #{tf}!"
+  exit 1
+end
+
 puts "Got #{tf_positions.size} positions from #{tf}."
 positions += tf_positions
 tf.logout!
@@ -21,6 +27,12 @@ puts "Logging in to #{sit}..."
 sit.login!
 puts "Getting positions from #{sit}..."
 sit_positions = sit.get_positions
+
+if sit_positions.size < 1
+  $stderr.puts "FAILED TO GET DATA FROM #{sit}!"
+  exit 1
+end
+
 puts "Got #{sit_positions.size} positions #{sit}."
 positions += sit_positions
 sit.logout!
@@ -31,3 +43,5 @@ filename = Time.now.strftime('%Y-%m-%dT%H:%M:%S%z')+'.yml'
 File.open(File.dirname(__FILE__)+'/data/'+filename, 'w') do |f|
   f.puts positions.to_yaml
 end
+
+puts "Total Value: $#{positions.inject(0){|tot,pos| tot + pos[:value]}}"
